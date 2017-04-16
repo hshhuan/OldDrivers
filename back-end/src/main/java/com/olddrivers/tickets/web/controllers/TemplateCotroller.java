cefa713d0b33a3b0a991507b30685ddc951b4c04
hshhuan <kegebo.h@gmail.com>
2017-04-16 23:21:51 +0800

    last update, permanentely moved to Organization OldDriversTeam

--- back-end/src/main/java/com/olddrivers/tickets/web/controllers/TemplateCotroller.java
+++ back-end/src/main/java/com/olddrivers/tickets/web/controllers/TemplateCotroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TemplateCotroller {
 
-	@RequestMapping({"/", "/index","/login","/regist","/profile"})
+	@RequestMapping({"/", "/index","/login","/regist"})
 	public String index() {
 		return "index";
 	}
